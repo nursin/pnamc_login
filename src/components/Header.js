b83cc71e62e508dee8debef2c9d35990c9640197
Bobby Keel <bobbykeel76@gmail.com>
2023-02-26 18:21:41 -0500

    login with email link

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,9 +38,53 @@ function Header() {
                 <NavbarBrand href="/">
                     <img src={pnamc_logo} className="navbar-logo-left" alt="PNAMC logo" />
                     <span className='text-white d-none d-md-block'>PNAMC</span>
-                    <div className="divider d-none d-lg-block"></div>
+                    <div className="divider d-none d-xl-block"></div>
                 </NavbarBrand>
-                <NavbarToggler onClick={() => setIsOpen(isOpen ? false : "isOpen")} />
+                <div className="d-flex align-items-center">
+                    <div className="d-block d-xl-none">
+                        {
+                            user
+                                ?
+                                user.photoURL
+                                    ?
+                                    <>
+                                        <div className="dropdown">
+                                            <div className='dropbtn'>
+                                                <img src={user?.photoURL} alt="Profile Pic" className="profileAvatar" />
+                                            </div>
+                                            <div className="dropdown-content">
+                                                {/* <NavLink onClick={() => setIsOpen(false)} to="/board-of-directors">Executive Board</NavLink> */}
+                                                <Link to="/account">Account</Link>
+                                                {/* <Link to="/messages">Transactions</Link>
+                                                <Link to="/contacts">Events</Link> */}
+                                                {user ? <a href='/' onClick={signOut}>Sign out</a> : <a href="/">Sign in</a>}
+                                            </div>
+                                        </div>
+                                    </>
+                                    :
+                                    <>
+                                        <div className="dropdown">
+                                            <div className='dropbtn'>
+                                                <p className='profileAvatar-noImage text-white fs-6'>{user?.email?.slice(0, 1).toUpperCase()}</p>
+                                            </div>
+                                            <div className="dropdown-content">
+                                                {/* <NavLink onClick={() => setIsOpen(false)} to="/board-of-directors">Executive Board</NavLink> */}
+                                                <Link to="/account">Account</Link>
+                                                {/* <Link to="/messages">Transactions</Link>
+                                            <Link to="/contacts">Events</Link> */}
+                                                {user ? <a href='/' onClick={signOut}>Sign out</a> : <a href="/">Sign in</a>}
+                                            </div>
+                                        </div>
+                                    </>
+                                :
+                                <>
+                                    {/* <JoinModal /> */}
+                                    <SigninModal />
+                                </>
+                        }
+                    </div>
+                    <NavbarToggler onClick={() => setIsOpen(isOpen ? false : "isOpen")} />
+                </div>
                 <Collapse isOpen={isOpen} navbar>
                     <Nav navbar className="justify-content-start" style={{ width: "100%" }}>
                         <div className="dropdown">
@@ -72,7 +116,7 @@ function Header() {
                                 <>
                                     <div className="dropdown">
                                         <div className='dropbtn'>
-                                           <img src={user?.photoURL} alt="Profile Pic" className="profileAvatar" /> 
+                                            <img src={user?.photoURL} alt="Profile Pic" className="profileAvatar" />
                                         </div>
                                         <div className="dropdown-content">
                                             {/* <NavLink onClick={() => setIsOpen(false)} to="/board-of-directors">Executive Board</NavLink> */}
@@ -84,7 +128,20 @@ function Header() {
                                     </div>
                                 </>
                                 :
-                                <p>{user?.displayName?.slice(0, 1)}</p>
+                                <>
+                                    <div className="dropdown">
+                                        <div className='dropbtn'>
+                                            <p className='profileAvatar-noImage text-white fs-6'>{user?.email?.slice(0, 1).toUpperCase()}</p>
+                                        </div>
+                                        <div className="dropdown-content">
+                                            {/* <NavLink onClick={() => setIsOpen(false)} to="/board-of-directors">Executive Board</NavLink> */}
+                                            <Link to="/account">Account</Link>
+                                            {/* <Link to="/messages">Transactions</Link>
+                                            <Link to="/contacts">Events</Link> */}
+                                            {user ? <a href='/' onClick={signOut}>Sign out</a> : <a href="/">Sign in</a>}
+                                        </div>
+                                    </div>
+                                </>
                             :
                             <>
                                 {/* <JoinModal /> */}
--- src/components/SigninModal.js
+++ src/components/SigninModal.js
@@ -3,7 +3,8 @@ import { Button, Col, Input, Modal, ModalBody, ModalFooter, ModalHeader, Progres
 import pnamc_logo from '../PNAMC_chapter_logo_transparent.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { auth, googleAuthProvider } from '../firebase';
+import { auth, googleAuthProvider, actionCodeSettings } from '../firebase';
+import firebase from 'firebase';
 
 // redux
 import { useDispatch, useSelector } from 'react-redux';
@@ -13,7 +14,26 @@ const memberApplication = "https://firebasestorage.googleapis.com/v0/b/pnamc-eb0
 
 function SigninModal() {
     const [isSigninModalOpen, setIsSigninModalOpen] = useState(false);
+    const [email, setEmail] = useState('');
 
+    const sendSignInLinkToEmail = () => {
+        auth.sendSignInLinkToEmail(email, actionCodeSettings)
+            .then(() => {
+                // The link was successfully sent. Inform the user.
+                // Save the email locally so you don't need to ask the user for it again
+                // if they open the link on the same device.
+                window.localStorage.setItem('emailForSignIn', email);
+                setIsSigninModalOpen(isSigninModalOpen ? false : true)
+                alert(`Follow the link sent to ${email} to complete login`);
+            })
+            .catch((error) => {
+                // link failed to send or work alert user
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert(errorCode);
+                alert(errorMessage);
+            });
+    }
     const dispatch = useDispatch();
 
     const signInWithGoogle = () => {
@@ -26,7 +46,7 @@ function SigninModal() {
 
     return (
         <>
-            <Button outline className='navbar__signin text-md-center fw-bold ms-2 py-1'
+            <Button outline className='navbar__signin text-md-center fw-bold mx-2 py-1'
                 onClick={() => setIsSigninModalOpen(isSigninModalOpen ? false : true)}
             >Login<FontAwesomeIcon icon="fa-regular fa-right-to-bracket" /></Button>
             <Modal
@@ -41,22 +61,31 @@ function SigninModal() {
                 <ModalBody className='d-flex flex-column pt-4'>
                     <img src={pnamc_logo} className="mx-auto" alt="PNAMC Logo" />
                     <p className='alt-p text-black text-center mt-3 mb-1'>Login/Join by clicking an option</p>
+                    <Input
+                        className='text-center round p-2 mx-auto'
+                        placeholder="Enter email address"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
                     <Button
-                        className='signin__googleFormButton mx-auto fw-bold'
+                        className='signin__email mx-auto fw-bold m-2'
+                        onClick={sendSignInLinkToEmail}
+                    >
+                        Continue with email
+                    </Button>
+                    <p className='text-black text-center fw-normal mb-2'>or</p>
+                    <Button
+                        className='signin__googleFormButton mx-auto fw-bold mb-2'
                         onClick={signInWithGoogle}
                     >
                         Continue with Google
                         <FontAwesomeIcon icon={faGoogle} className="fs-5 ms-2" />
                     </Button>
-                    <Button
-                        className='signin__email mx-auto fw-bold  m-3'
-                    >
-                        Continue with email
-                    </Button>
                     {/* takes user to the create  */}
                     {/* <p className='text-black text-center mt-4 mb-0'>Dont have an account?</p>
                     <a href='' className='alt-text text-center mx-auto'>Create an account</a> */}
-                    <p className='alt-p text-black text-center mb-0'>Other options below</p>
+                    <hr />
+                    <p className='alt-p text-black text-center mb-0 mt-2'>Other options below</p>
                     <a
                         className='alt-text mx-auto'
                         href={memberApplication}
